77471929bad571e312eef30c6524d49607bb0881
stephen williams <stephen.ray.williams@gmail.com>
2015-04-05 15:35:52 -0500

    Requirements met. Added more comments.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,8 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 INEW 2438 Project 2 Assignment
 Stephen R. Williams
 
-Create a REST service that do the following. 
+REST service using Spring
+This class and main method use SpringApplication.run to set up the service without web.xml
+Reference: https://spring.io/guides/gs/rest-service/
 
+Assignment text:
+Create a REST service that do the following.
 Create two REST services that use path parameter and query parameter.
     the two methods should  collect your favorite websites' url, and  category.
     Create a class that captures the favorite websites' url, and  category.
@@ -16,6 +20,8 @@ Create two REST services that use path parameter and query parameter.
 Create a REST API that displays the content of the updated file upon request.
 The program should be testable by calling the REST APIs directly from any browser
 Upload your project in GitHub and just submit the url in a word/text document
+
+
 */
 
 
